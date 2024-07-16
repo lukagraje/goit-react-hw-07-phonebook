6f2f0a80b8581e0270b1ae2893b09654b2304a58
lukagraje <138776692+lukagraje@users.noreply.github.com>
2024-07-16 15:35:52 +0200

    Homework

--- src/components/Contacts/App.jsx
+++ src/components/Contacts/App.jsx
@@ -1,27 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { nanoid } from "nanoid";
 import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
 import Filter from "./SearchFilter/Filter";
-import { addContact, deleteContact } from "../../redux/slices/contactsSlice";
+import {
+  fetchContacts,
+  addContact,
+  deleteContact,
+} from "../../redux/slices/contactsSlice";
 import { setFilter } from "../../redux/slices/filtersSlice";
 
 export default function App() {
-  const contacts = useSelector((state) => state.contacts);
+  const contacts = useSelector((state) => state.contacts.items);
   const filter = useSelector((state) => state.filter);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const storedContacts = JSON.parse(localStorage.getItem("contacts"));
-    if (storedContacts && storedContacts.length > 0) {
-      storedContacts.forEach((contact) => dispatch(addContact(contact)));
-    }
+    dispatch(fetchContacts());
   }, [dispatch]);
 
-  useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   const storedContacts = JSON.parse(localStorage.getItem("contacts"));
+  //   if (storedContacts && storedContacts.length > 0) {
+  //     storedContacts.forEach((contact) => dispatch(addContact(contact)));
+  //   }
+  // }, [dispatch]);
+
+  // useEffect(() => {
+  //   localStorage.setItem("contacts", JSON.stringify(contacts));
+  // }, [contacts]);
 
   const handleAddContact = (name, number) => {
     const newContact = { name, number, id: nanoid() };
